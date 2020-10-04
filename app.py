04e367ad99a27a6f78e72964b4177332c91d4f3b
takitsu21 <dylannbatisse@gmail.com>
2020-10-04 23:42:52 +0200

    .vscode

--- app.py
+++ app.py
@@ -350,15 +350,15 @@ def index_api_version():
     return jsonify(route_homepage)
 
 
-# if __name__ == '__main__':
-#     try:
-#         app.run(debug=True, host="0.0.0.0", port=5000)
-#     except KeyboardInterrupt:
-#         with app.app_context():
-#             cache.clear()
-#         exit(0)
-#     except Exception as e:
-#         print(type(e).__name__, e)
-#         with app.app_context():
-#             cache.clear()
-#         exit(1)
+if __name__ == '__main__':
+    try:
+        app.run(debug=True, host="0.0.0.0", port=5000)
+    except KeyboardInterrupt:
+        with app.app_context():
+            cache.clear()
+        exit(0)
+    except Exception as e:
+        print(type(e).__name__, e)
+        with app.app_context():
+            cache.clear()
+        exit(1)
